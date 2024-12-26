b72815da78237da8b216360be37765f0f2899679
keenetic29 <al29.andreev@gmail.com>
2024-12-26 21:23:50 +0300

    Update

--- pkg/model/models.go
+++ pkg/model/models.go
@@ -1,9 +1,11 @@
 package model;
 
 type Product struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
-	Quantity    int     `json:"quantity"`
+	ID          	int     `json:"id"`
+	Name        	string  `json:"name"`
+	Description 	string  `json:"description"`
+	Cost       		float64 `json:"cost"`
+	Count			int     `json:"count"`
+	ManufacturerId 	int 	`json:"manufacturerId"`
+	SupplierId		int 	`json:"supplierId"`
 }
--- pkg/repository/products.go
+++ pkg/repository/products.go
@@ -3,9 +3,9 @@ package repository
 import "rest.com/pkg/model"
 
 var products = []model.Product{
-	{ID: 1, Name: "Телефон", Description: "Смартфон с 5G", Price: 50000, Quantity: 100},
-	{ID: 2, Name: "Ноутбук", Description: "Ультрабук для работы", Price: 75000, Quantity: 50},
-	{ID: 3, Name: "Планшет", Description: "Компактный и удобный", Price: 30000, Quantity: 60},
+	{ID: 1, Name: "Телефон", Description: "Смартфон с 5G", Cost: 50000, Count: 100, ManufacturerId: 1, SupplierId: 1},
+	{ID: 2, Name: "Ноутбук", Description: "Ультрабук для работы", Cost: 75000, Count: 50, ManufacturerId: 1, SupplierId: 1},
+	{ID: 3, Name: "Планшет", Description: "Компактный и удобный", Cost: 30000, Count: 60, ManufacturerId: 1, SupplierId: 1},
 }
 
 
